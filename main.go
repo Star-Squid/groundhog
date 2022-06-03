ac75c9556408842bb9cb315f5bccde526b0cd021
Barbara Malta <barbara@mb-malta.co.uk>
2022-06-03 08:16:45 +0100

    Add cats and dogs

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
-func main ()  {
+func main() {
+
+	var cat, dog string
+	cat = "Kalmar"
+	dog = "Burek"
+
 	fmt.Println("hi")
+	fmt.Printf("Are you a %v or a %v person?", cat, dog)
 }
