3e89d9923d36ad2102859a50d01aba0bf3890c38
Barbara Malta <barbara@mb-malta.co.uk>
2022-06-03 11:32:38 +0100

    turn it into a question machine

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import "fmt"
 
+// func main() {
+
+// 	var cat, dog string
+// 	cat = "Kalmar"
+// 	dog = "Burek"
+// 	statement := fmt.Sprintf("I like %v more than %v. \n", cat, dog)
+
+// 	fmt.Println("Hi there!")
+// 	fmt.Printf("Are you a %s or a %v person?\n", cat, dog)
+// 	fmt.Print(statement)
+// }
+
 func main() {
+	var name string
 
-	var cat, dog string
-	cat = "Kalmar"
-	dog = "Burek"
-
-	fmt.Println("hi")
-	fmt.Printf("Are you a %v or a %v person?", cat, dog)
+	fmt.Println("What is your name?")
+	fmt.Scan(&name)
+	fmt.Printf("Hello, %v!\n", name)
 }
